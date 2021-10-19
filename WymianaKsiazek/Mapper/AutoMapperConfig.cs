7a948d371b6ef03bad2c1b00d941b3edf79fda4c
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-10-19 18:27:25 +0200

    Add files via upload
    AutoMapper Config File

---
+++ WymianaKsiazek/Mapper/AutoMapperConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WymianaKsiazek.Database.Entities;
+using WymianaKsiazek.Models.MapperModels;
+
+namespace WymianaKsiazek.Mapper
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+            => new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<OfferEntity, OffersListMP>();
+                cfg.CreateMap<OfferEntity, OfferMP>();
+                cfg.CreateMap<AddressEntity, AddressMP>();
+                cfg.CreateMap<BookEntity, BookMP>();
+                cfg.CreateMap<BookEntity, BookOpinionMP>();
+                cfg.CreateMap<BookOpinionsEntity, BookOpinionsMP>();
+                cfg.CreateMap<CategoryEntity, CategoryMP>();
+                cfg.CreateMap<OfferCommentsEntity, OfferCommentsMP>();
+                cfg.CreateMap<BookCommentsEntity, BookCommentsMP>();
+                cfg.CreateMap<UserEntity, UserMP>();
+                cfg.CreateMap<UserEntity, MyProfileMP>();
+                cfg.CreateMap<UserEntity, UserProfile>();
+            }).CreateMapper();
+    }
+}
