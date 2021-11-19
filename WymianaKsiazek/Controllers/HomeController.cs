8e585b8903c294ea7b7f43fe2aaf3f851c6c93f1
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 18:14:18 +0100

    Update HomeController.cs

--- WymianaKsiazek/Controllers/HomeController.cs
+++ WymianaKsiazek/Controllers/HomeController.cs
@@ -1,38 +1,74 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using WymianaKsiazek.Database;
 using WymianaKsiazek.Models;
+using WymianaKsiazek.Queries.OfferQueries;
+using WymianaKsiazek.Database.Entities;
+using WymianaKsiazek.Queries.BookQueries;
+using System.Dynamic;
+using WymianaKsiazek.Queries.UserQueries;
 
 namespace WymianaKsiazek.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Context _context;
+        private readonly IOfferQueries _offerQueries;
+        private readonly IBookQueries _bookQueries;
+        private readonly IUserQueries _userQueries;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Context context, IOfferQueries offerQueries, IBookQueries bookQueries
+            , IUserQueries userQueries)
         {
             _logger = logger;
+            _context = context;
+            _offerQueries = offerQueries;
+            _bookQueries = bookQueries;
+            _userQueries = userQueries;
         }
 
         public IActionResult Index()
         {
-            ViewBag.IsUserLoggedIn = IsUserLoggedIn();
-            return View();
+            bool isuerloggedin = IsUserLoggedIn();
+            var offers = _offerQueries.GetAllOffers();
+            ViewBag.IsUserLoggedIn = isuerloggedin;
+            return View(offers);
         }
-
-        public IActionResult Privacy()
+        public IActionResult Category(long id)
         {
-            return View();
+            var categoryname = _bookQueries.GetCategoryName(id);
+            if(categoryname == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            bool isuerloggedin = IsUserLoggedIn();
+            ViewBag.IsUserLoggedIn = isuerloggedin;
+            ViewBag.CategoryName = categoryname;
+            var books = _bookQueries.GetBooksByCategory(id);
+            var offers = _offerQueries.GetOffersByCategory(id);
+            dynamic model = new ExpandoObject();
+            model.Offers = offers;
+            model.Books = books;
+            return View(model);
         }
-
+        public IActionResult Test()
+        {
+            ViewBag.IsUserLoggedIn = IsUserLoggedIn();
+            var offer = _offerQueries.GetOfferById(1);
+            return View(offer);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            ViewBag.IsUserLoggedIn = IsUserLoggedIn();
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
         private bool IsUserLoggedIn()
@@ -44,6 +80,12 @@ namespace WymianaKsiazek.Controllers
                 p = true;
             }
             return p;
+        }
+        private string GetUserId()
+        {
+            var refreshtoken = HttpContext.Session.GetString("RefreshToken");
+            string userid = _context.RefreshTokens.Where(x => x.Token == refreshtoken).Select(x => x.UserId).FirstOrDefault();
+            return userid;
         }
     }
 }
