09d205fd8db4e8f46b0d239a1cd52dfead87a0a8
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 17:53:22 +0100

    Edit Book Queries Interface - First Version

--- WymianaKsiazek/Queries/BookQueries/IBookQueries.cs
+++ WymianaKsiazek/Queries/BookQueries/IBookQueries.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,16 @@ namespace WymianaKsiazek.Queries.BookQueries
 {
     public interface IBookQueries
     {
+        public List<BookReviewsList> GetBooks();
+        public List<BookReviewsList> GetBooksByCategory(long id);
+        public List<BookReviewsList> GetBooksByAuthor(string author);
+        public List<BookReviewsList> GetConcretBook(string title, string author);
+        public List<BookReviewsList> GetSearchedBooks(string title, string author);
+        public List<BookReviewsList> GetSearchedBooksByFilter(List<BookReviewsList> books, long categoryid, int sorttype);
+        public BookOpinionMP GetBookById(long id);
         public BookOpinionMP GetBookOpinions(long bookid);
+        public string GetCategoryName(long id);
+        public int GetUserOpinionAboutBook(long opinionid, string userid);
+        public Task AddUserOpinionAboutBook(int value, long opinionid, string userid);
     }
 }
