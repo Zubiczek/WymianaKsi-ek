3696b6fd94b769fbd285c789a32b297ef5998a06
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 18:11:14 +0100

    Update UserEntity.cs

--- WymianaKsiazek/Database/Entities/UserEntity.cs
+++ WymianaKsiazek/Database/Entities/UserEntity.cs
@@ -14,12 +14,23 @@ namespace WymianaKsiazek.Database.Entities
         public virtual ICollection<OfferCommentsEntity> OfferComments { get; set; }
         public virtual ICollection<BookCommentsEntity> BookComments { get; set; }
         public virtual ICollection<UserBookOpinion> UserOpinions { get; set; }
+        public virtual ICollection<UserUserOpinionEntity> UserUserOpinion { get; set; }
+        public virtual UserOpinionsEntity UserOpinion { get; set; }
+        public virtual ICollection<UserLikedOffersEntity> LikedOffers { get; set; }
+        public virtual ICollection<ConversationEntity> MyConversations { get; set; }
+        public virtual ICollection<ConversationEntity> ConversationsWith { get; set; }
+        public virtual ICollection<MessageEntity> Messages { get; set; }
         public UserEntity()
         {
             Offers = new List<OfferEntity>();
             OfferComments = new List<OfferCommentsEntity>();
             BookComments = new List<BookCommentsEntity>();
             UserOpinions = new List<UserBookOpinion>();
+            UserUserOpinion = new List<UserUserOpinionEntity>();
+            LikedOffers = new List<UserLikedOffersEntity>();
+            MyConversations = new List<ConversationEntity>();
+            ConversationsWith = new List<ConversationEntity>();
+            Messages = new HashSet<MessageEntity>();
         }
     }
 }
