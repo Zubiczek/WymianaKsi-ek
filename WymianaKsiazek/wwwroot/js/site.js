c0f8b1c52ad82b747a82664c28b26f3e2f6b93c0
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 18:18:42 +0100

    Add new scripts #1

--- WymianaKsiazek/wwwroot/js/site.js
+++ WymianaKsiazek/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
@@ -15,4 +15,58 @@ $(function () {
 	}, function () {
 		$('.user-nickname').hide().text(nickname).fadeIn("slow");
 	});
+});
+
+$('#user-opinion1').hover(function () {
+	$(this).css("color", "orange");
+}, function () {
+	$(this).css("color", "black");
+});
+
+$('#user-opinion2').hover(function () {
+	for (let i = 1; i <= 2; i++) {
+		let divid = "#user-opinion" + i;
+		$(divid).css("color", "orange");
+	}
+}, function () {
+	for (let i = 1; i <= 2; i++) {
+		let divid = "#user-opinion" + i;
+		$(divid).css("color", "black");
+	}
+});
+
+$('#user-opinion3').hover(function () {
+	for (let i = 1; i <= 3; i++) {
+		let divid = "#user-opinion" + i;
+		$(divid).css("color", "orange");
+	}
+}, function () {
+	for (let i = 1; i <= 3; i++) {
+		let divid = "#user-opinion" + i;
+		$(divid).css("color", "black");
+	}
+});
+
+$('#user-opinion4').hover(function () {
+	for (let i = 1; i <= 4; i++) {
+		let divid = "#user-opinion" + i;
+		$(divid).css("color", "orange");
+	}
+}, function () {
+	for (let i = 1; i <= 4; i++) {
+		let divid = "#user-opinion" + i;
+		$(divid).css("color", "black");
+	}
+});
+
+$('#user-opinion5').hover(function () {
+	for (let i = 1; i <= 5; i++) {
+		let divid = "#user-opinion" + i;
+		$(divid).css("color", "orange");
+	}
+}, function () {
+	for (let i = 1; i <= 5; i++) {
+		let divid = "#user-opinion" + i;
+		$(divid).css("color", "black");
+	}
 });
