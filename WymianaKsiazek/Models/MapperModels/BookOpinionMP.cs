a525e7dd001a4d1474e0d5ef3e4b2fbf085e52b3
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 17:56:39 +0100

    Update BookOpinionMP.cs

--- WymianaKsiazek/Models/MapperModels/BookOpinionMP.cs
+++ WymianaKsiazek/Models/MapperModels/BookOpinionMP.cs
@@ -11,8 +11,8 @@ namespace WymianaKsiazek.Models.MapperModels
         public long Book_Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        public string Img { get; set; }
         public virtual BookOpinionsMP Opinion { get; set; }
-        public virtual ICollection<OffersListMP> Offers { get; set; }
         public virtual ICollection<BookCommentsMP> Comments { get; set; }
     }
 }
