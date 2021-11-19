5606f9fccb2208a6280cbaa5255917ab5a1c1e9e
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 17:55:10 +0100

    Add AddOffer model

---
+++ WymianaKsiazek/Models/AddOffer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WymianaKsiazek.Models
+{
+    public class AddOffer
+    {
+        public string titleinput { get; set; }
+        public string authorinput { get; set; }
+        public long inputcategoryid { get; set; }
+        public string addoffercontent {get; set;}
+        public int forsale { get; set; }
+        public string price { get; set; }
+        public IFormFile addofferimg { get; set; }
+    }
+}
