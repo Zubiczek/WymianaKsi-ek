79d7b57e9268958dc486f07b840907354a5e5837
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 17:57:17 +0100

    Update BookOpinionsMP.cs

--- WymianaKsiazek/Models/MapperModels/BookOpinionsMP.cs
+++ WymianaKsiazek/Models/MapperModels/BookOpinionsMP.cs
@@ -13,5 +13,29 @@ namespace WymianaKsiazek.Models.MapperModels
         public uint Opinion3 { get; set; }
         public uint Opinion2 { get; set; }
         public uint Opinion1 { get; set; }
+        public uint TotalOpinions()
+        {
+            return this.Opinion1 + this.Opinion2 + this.Opinion3 + this.Opinion4 + this.Opinion5;
+        }
+        public uint AverageOpinion()
+        {
+            uint sumofopinions = (5 * this.Opinion5) + (4 * this.Opinion4) + (3 * this.Opinion3) + (2 * this.Opinion2) + this.Opinion1;
+            uint totalopinions = this.TotalOpinions();
+            if(totalopinions == 0)
+            {
+                return 0;
+            }
+            double average = (double)sumofopinions / totalopinions;
+            return Convert.ToUInt32(Math.Round(average));
+        }
+        public (uint, uint, uint, uint, uint) OpinionsWithPercentage()
+        {
+            uint totalopinions = this.TotalOpinions();
+            if(totalopinions == 0)
+            {
+                return (0,0,0,0,0);
+            }
+            return ((this.Opinion5 * 100) / totalopinions, (this.Opinion4 * 100) / totalopinions, (this.Opinion3 * 100) / totalopinions, (this.Opinion2 * 100) / totalopinions, (this.Opinion1 * 100) / totalopinions);
+        }
     }
 }
