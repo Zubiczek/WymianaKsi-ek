58151f944376f4334f75ee717bdc04f2df7a1f05
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 17:50:28 +0100

    Edit UserQueries Interface - First Version

--- WymianaKsiazek/Queries/UserQueries/IUserQueries.cs
+++ WymianaKsiazek/Queries/UserQueries/IUserQueries.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,12 +10,23 @@ namespace WymianaKsiazek.Queries.UserQueries
 {
     public interface IUserQueries
     {
+        public Task<UserMP> GetUserById(string id); 
         public Task<int> CreateNewUser(CreateUserMP model);
         public (string, DateTime) CreateAccessToken(UserEntity user);
         public Task<string> CreateRefreshToken(string userId);
         public Task<TokenOutput> CreateToken(string email, string password);
         public Task<int> RemoveRefreshToken(string token);
         public MyProfileMP MyProfile(string userid);
-
+        public Task SendEmailResetPassword(UserEntity user);
+        public Task ChangePassword(UserEntity user, string password);
+        public Task<int> ChangeCurrentPassword(string userid, string oldpasswd, string newpasswd);
+        public UserProfile GetUserProfile(string id);
+        public int GetUserOpinionAboutUser(string userprofileid, string usersopinionid);
+        public Task AddUserOpinionAboutUser(int value, long opinion_id, string userid);
+        public Task LikeOffer(string userid, long offerid);
+        public Task UnLikeOffer(string userid, long offerid);
+        public List<OffersListMP> GetUsersLikedOffers(string userid);
+        public ConversationMP GetConversationById(long id);
+        public Task CreateMessage(long convid, string text, string userid);
     }
 }
