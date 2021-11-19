4b1e13744b5f67d9086447060b5bc6ac6ab903c7
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-19 17:51:07 +0100

    Edit UserQueries - Add Implementation of Interface

--- WymianaKsiazek/Queries/UserQueries/UserQueries.cs
+++ WymianaKsiazek/Queries/UserQueries/UserQueries.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -46,6 +46,11 @@ namespace WymianaKsiazek.Queries.UserQueries
             _mailService = mailService;
             _urlHelper = urlHelper;
             _request = httpContextAccessor.HttpContext.Request;
+        }
+        public async Task<UserMP> GetUserById(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            return _mapper.Map<UserMP>(user);
         }
         public async Task<int> CreateNewUser(CreateUserMP model)
         {
@@ -201,9 +206,146 @@ namespace WymianaKsiazek.Queries.UserQueries
         }
         public MyProfileMP MyProfile(string userid)
         {
-            var userdb = _context.User.Where(x => x.Id == userid).FirstOrDefault();
+            var userdb = _context.User.Include(x => x.Offers).ThenInclude(x => x.Book).Include(x => x.Offers).ThenInclude(x => x.User)
+                .Include(x => x.Offers).ThenInclude(x => x.Address).Where(x => x.Id == userid).FirstOrDefault();
             return _mapper.Map<MyProfileMP>(userdb);
-
+        }
+        public async Task SendEmailResetPassword(UserEntity user)
+        {
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var confirmationlink = _urlHelper.Action("ResetPassword", "User", new { userId = user.Id, token = token }, _request.Scheme);
+            MailRequest mailRequest = new MailRequest();
+            mailRequest.ToEmail = user.Email;
+            mailRequest.Subject = "wymienksiazke.com - Reset hasła - Weryfikacja";
+            string mailtext = "<div style = 'background-color: #696969; color: white; margin-left: auto; margin-right: auto; width: 100%;'>" +
+            "<div style = 'padding-top: 30px; text-align: center;'>" +
+                "Reset hasła - Weryfikacja!" +
+            "</div>" +
+            "<div style = 'padding-top: 30px; text-align: center;'>" +
+                "Kliknij poniższy przycisk zresetować hasło" +
+            "</div>" +
+            "<div style = 'background-color: #228B22; padding: 10px; width: 100px; margin-left: 42%; margin-top: 30px;'>" +
+                "<a href = '" + confirmationlink + "' style = 'text-decoration:none'>" +
+                    "<div style = 'text-align: center; color: white;'>" +
+                        "Resetuj" +
+                    "</div>" +
+                "</a>" +
+            "</div>" +
+            "<div style = 'background-color: black; text-align: center; margin-top: 20px; color: white;'>" +
+                "wymienksiazke.com &copy Wszelkie prawa zastrzeżone!" +
+            "</div>" +
+            "</div>";
+            mailRequest.Body = mailtext;
+            await _mailService.SendEmailAsync(mailRequest);
+        }
+        public async Task<int> ChangeCurrentPassword(string userid, string oldpasswd, string newpasswd)
+        {
+            var user = await _userManager.FindByIdAsync(userid);
+            if(user == null)
+            {
+                return 1;
+            }
+            var signInResult = await _signInManager.CheckPasswordSignInAsync(user, oldpasswd, false);
+            if (!signInResult.Succeeded)
+            {
+                return 2;
+            }
+            await ChangePassword(user, newpasswd);
+            return 0;
+        }
+        public async Task ChangePassword(UserEntity user, string password)
+        {
+            var passwordHash = new PasswordHasher<UserEntity>();
+            var hashed = passwordHash.HashPassword(user, password);
+            user.PasswordHash = hashed;
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                transaction.Commit();
+            }
+        }
+        public UserProfile GetUserProfile(string id)
+        {
+            var user = _context.User.Include(x => x.UserOpinion).Include(x => x.Offers).ThenInclude(x => x.Book).
+                Include(x => x.Offers).ThenInclude(x => x.Address).Where(x => x.Id == id).FirstOrDefault();
+            return _mapper.Map<UserProfile>(user);
+        }
+        public int GetUserOpinionAboutUser(string userprofileid, string usersopinionid)
+        {
+            var opinionid = _context.UserOpinions.Where(x => x.User_Id == userprofileid).Select(x => x.Opinion_Id).FirstOrDefault();
+            if(opinionid == 0)
+            {
+                return -1;
+            }
+            var useropinion = 0;
+            useropinion = _context.UserOpinionInfo.Where(x => x.Opinion_Id == opinionid && x.User_Id == usersopinionid).Select(x => x.Value).FirstOrDefault();
+            return useropinion;
+        }
+        public async Task AddUserOpinionAboutUser(int value, long opinion_id, string userid)
+        {
+            UserUserOpinionEntity useropinion = new UserUserOpinionEntity();
+            useropinion.User_Id = userid;
+            useropinion.Opinion_Id = opinion_id;
+            useropinion.Value = value;
+            using(var transaction = _context.Database.BeginTransaction())
+            {
+                _context.Add(useropinion);
+                var useropiniontable = _context.UserOpinions.Where(x => x.Opinion_Id == opinion_id).FirstOrDefault();
+                if(useropiniontable != null)
+                {
+                    useropiniontable.OpinionSumValue += (uint)value;
+                    useropiniontable.TotalOpinions += 1;
+                }
+                await _context.SaveChangesAsync();
+                transaction.Commit();
+            }
+        }
+        public async Task LikeOffer(string userid, long offerid)
+        {
+            UserLikedOffersEntity like = new UserLikedOffersEntity();
+            like.User_Id = userid;
+            like.Offer_Id = offerid;
+            using(var transaction = _context.Database.BeginTransaction())
+            {
+                _context.Add(like);
+                await _context.SaveChangesAsync();
+                transaction.Commit();
+            }
+        }
+        public async Task UnLikeOffer(string userid, long offerid)
+        {
+            var like = _context.UserLikedOffers.Where(x => x.User_Id == userid && x.Offer_Id == offerid).FirstOrDefault();
+			if(like != null)
+			{
+				_context.UserLikedOffers.Attach(like);
+				_context.UserLikedOffers.Remove(like);
+				await _context.SaveChangesAsync();
+			}
+        }
+        public List<OffersListMP> GetUsersLikedOffers(string userid)
+        {
+            var offers = _context.UserLikedOffers.Include(x => x.Offer).ThenInclude(x => x.Book).Include(x => x.Offer).ThenInclude(x => x.User)
+                .Include(x => x.Offer).ThenInclude(x => x.Address).Where(x => x.User_Id == userid).Select(x => x.Offer).ToList();
+            return _mapper.Map<List<OffersListMP>>(offers);
+        }
+        public ConversationMP GetConversationById(long id)
+        {
+            var conversation = _context.Conversations.Include(x => x.User1).Include(x => x.User2).Include(x => x.Messages)
+                .Where(x => x.Id == id).FirstOrDefault();
+            return _mapper.Map<ConversationMP>(conversation);
+        }
+        public async Task CreateMessage(long convid, string text, string userid)
+        {
+            MessageEntity message = new MessageEntity();
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                message.Text = text;
+                message.Conv_Id = convid;
+                message.User_Id = userid;
+                await _context.AddAsync(message);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
