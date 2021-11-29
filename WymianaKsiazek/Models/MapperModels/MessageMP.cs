e78975aae754b54c5160801d6e7817fcf9e26e74
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-29 18:10:11 +0100

    Add Message DTO models

---
+++ WymianaKsiazek/Models/MapperModels/MessageMP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WymianaKsiazek.Models.MapperModels
+{
+    public class MessageMP
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public DateTime SendDate { get; set; }
+        public virtual UserMP User { get; set; }
+    }
+}
---
+++ WymianaKsiazek/Models/MapperModels/UserConversationsMP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WymianaKsiazek.Models.MapperModels
+{
+    public class UserConversationsMP
+    {
+        public long Id { get; set; }
+        public virtual UserMP User1 { get; set; }
+        public virtual UserMP User2 { get; set; }
+        public bool IsAllMessagesRead { get; set; }
+        public string LastMessageFrom_UserId { get; set; }
+        public LastMessageMP LastMessage { get; set; }
+    }
+}
