4cb587f1560b4c645dcdb15c077c2a017df5bd28
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-29 18:18:30 +0100

    Update IUserQueries.cs

--- WymianaKsiazek/Queries/UserQueries/IUserQueries.cs
+++ WymianaKsiazek/Queries/UserQueries/IUserQueries.cs
@@ -21,12 +21,6 @@ namespace WymianaKsiazek.Queries.UserQueries
         public Task ChangePassword(UserEntity user, string password);
         public Task<int> ChangeCurrentPassword(string userid, string oldpasswd, string newpasswd);
         public UserProfile GetUserProfile(string id);
-        public int GetUserOpinionAboutUser(string userprofileid, string usersopinionid);
-        public Task AddUserOpinionAboutUser(int value, long opinion_id, string userid);
-        public Task LikeOffer(string userid, long offerid);
-        public Task UnLikeOffer(string userid, long offerid);
         public List<OffersListMP> GetUsersLikedOffers(string userid);
-        public ConversationMP GetConversationById(long id);
-        public Task CreateMessage(long convid, string text, string userid);
     }
 }
