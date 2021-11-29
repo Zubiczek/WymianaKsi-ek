ba523389b26b2cb36ef7e3bea1d72083d39903a9
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-29 18:08:32 +0100

    Update UserMP.cs

--- WymianaKsiazek/Models/MapperModels/UserMP.cs
+++ WymianaKsiazek/Models/MapperModels/UserMP.cs
@@ -9,5 +9,6 @@ namespace WymianaKsiazek.Models.MapperModels
     {
         public string Id { get; set; }
         public string UserName { get; set; }
+        public string Img { get; set; }
     }
 }
