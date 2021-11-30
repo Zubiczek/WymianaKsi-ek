49eb5512aec9419314289371e39a65589c2df42f
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-11-30 19:19:15 +0100

    Add OfferReport DTO model

---
+++ WymianaKsiazek/Models/MapperModels/OfferReportMP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WymianaKsiazek.Models.MapperModels
+{
+    public class OfferReportMP
+    {
+        public long Offer_Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Image { get; set; }
+        public virtual BookMP Book { get; set; }
+        public virtual UserMP User { get; set; }
+    }
+}
