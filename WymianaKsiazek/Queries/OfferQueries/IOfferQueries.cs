4991290e6626e7c441200d712b00058131f23fb0
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-02 14:09:20 +0100

    Update IOfferQueries.cs

--- WymianaKsiazek/Queries/OfferQueries/IOfferQueries.cs
+++ WymianaKsiazek/Queries/OfferQueries/IOfferQueries.cs
@@ -10,19 +10,19 @@ namespace WymianaKsiazek.Queries.OfferQueries
 {
     public interface IOfferQueries
     {
-        public List<OffersListMP> GetOffers();
-        public List<OffersListMP> GetAllOffers();
-        public List<OffersListMP> GetOffersWithBook(string title, string author);
-        public List<OffersListMP> GetCityOffers(long addressid);
-        public List<OffersListMP> GetUserOffers(string userid);
-        public List<OffersListMP> GetSearchedOffers(string bookname);
-        public List<OffersListMP> GetOffersByCategory(long id);
-        public OfferMP GetOfferById(long offerid);
-        public List<OffersListMP> GetSearchesOffers(string titleauthor, string city);
-        public List<OffersListMP> GetSearchedOffersByFilters(List<OffersListMP> offers, long categoryid, uint lowprice, uint upprice, int type);
-        public List<AddBookMP> GetBooksToAdd(string title);
-        public bool IsContentValid(string content);
-        public string SaveImage(IFormFile image);
-        public Task<int> AddOffer(AddOffer offer, string userid);
+        Task<List<OffersListMP>> GetOffers();
+        Task<List<OffersListMP>> GetAllOffers();
+        Task<List<OffersListMP>> GetOffersWithBook(string title, string author);
+        Task<List<OffersListMP>> GetUserOffers(string userid);
+        Task<List<OffersListMP>> GetSearchedOffers(string bookname);
+        Task<List<OffersListMP>> GetOffersByCategory(long id);
+        Task<OfferMP> GetOfferById(long offerid);
+        Task<List<OffersListMP>> GetSearchesOffers(string titleauthor, string city);
+        List<OffersListMP> GetSearchedOffersByFilters(List<OffersListMP> offers, long categoryid, uint lowprice, uint upprice, int type);
+        Task<List<AddBookMP>> GetBooksToAdd(string title);
+        bool IsContentValid(string content);
+        string SaveImage(IFormFile image);
+        Task<int> AddOffer(AddOffer offer, string userid);
+        Task<OfferReportMP> GetOfferReport(long id);
     }
 }
