3fdec8a1686f6fe7bd283eb50c47b33a3cf11182
Zubiczek <56606960+Zubiczek@users.noreply.github.com>
2021-12-02 14:05:28 +0100

    Add SaveImg

---
+++ WymianaKsiazek/Functions/SaveImg.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WymianaKsiazek.Functions
+{
+    public class SaveImg : ISaveImg
+    {
+        public string SaveImage(IFormFile image)
+        {
+            if (image != null)
+            {
+                string imageName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                string SavePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", imageName);
+                using (var stream = new FileStream(SavePath, FileMode.Create))
+                {
+                    image.CopyTo(stream);
+                }
+                return imageName;
+            }
+            return null;
+        }
+    }
+}
